6d712174694e0afc5d3b2de7ed242fb8979d6745
graham <graham@arylla.com>
2017-10-24 09:00:11 -0400

    Unknown changes

--- PythonApplication1/fun.py
+++ PythonApplication1/fun.py
@@ -1,5 +1,7 @@
 #functions
 import numpy as np
+import cv2
+import matplotlib.pyplot as plt
 
 #converts nan to binary where nan values are 0 and non-nan values are 1
 def nanTObin(input_mat,flipper=False):
@@ -19,9 +21,12 @@ def nanTObin(input_mat,flipper=False):
 
 #makes a radially symmetrical gaussian filter based on input image dimentions
 def gauss_maker(fig_rows,fig_cols,sigma_perc,anchor_perc_rows,anchor_perc_cols,max_val=1,flipper=False):
-    #initialize sigma
+    #initialize sigma and k
     sigma=np.sqrt(fig_rows**2+fig_cols**2)*sigma_perc
-
+    
+    k=int(max([fig_rows,fig_cols]))*2
+    sigma_fix=0.3*((k-1)*0.5-1)+0.8
+    print str(sigma)+','+str(sigma_fix)
     #Calculate kernel
     kernel_1d=cv2.getGaussianKernel(k,sigma)
     kernel_1d=kernel_1d/max(kernel_1d)*max_val
@@ -42,3 +47,12 @@ def gauss_maker(fig_rows,fig_cols,sigma_perc,anchor_perc_rows,anchor_perc_cols,m
         final_fig=-(filler_fig-max_val)
         filler_fig=final_fig
     return filler_fig
+
+#automatically makes subplots from a list of mats
+def plotter(input_list):
+    numberOFplots=len(input_list)
+    for i in np.arange(numberOFplots):
+        plt.subplot(1,numberOFplots,i+1)
+        plt.imshow(input_list[i])
+    plt.show()
+    return
