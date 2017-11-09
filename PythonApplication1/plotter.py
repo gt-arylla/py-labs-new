eb0edcc4e7097ad5a06d41f4ab8c9a1a02adb8ca
graham <graham@arylla.com>
2017-11-09 17:58:28 -0500

    Save state before adding column info

--- PythonApplication1/plotter.py
+++ PythonApplication1/plotter.py
@@ -6,7 +6,7 @@ import itertools as it
 
 
 #Plot data from input CSV, organized according to the FAF C++ code
-def FAF_plotter(csv_file,x_col,y_col,s_col):
+def FAF_plotter(csv_file,x_col_in,y_col_in,s_col,yaxis=[],xaxis=[]):
     #Define Variables
     #x_col=[5]
     #y_col=[7]
@@ -31,8 +31,11 @@ def FAF_plotter(csv_file,x_col,y_col,s_col):
     with open(csv_file) as csvfile:
         readCSV=csv.reader(csvfile,delimiter=',')
         for row in readCSV:
-            all_data[row_count,:]=row
-            row_count+=1
+            try:
+                all_data[row_count,:]=row
+                row_count+=1
+            except:
+                print "Row Import Failed"
     print all_data
 
     '''
@@ -83,68 +86,88 @@ def FAF_plotter(csv_file,x_col,y_col,s_col):
     
     print super_list
 
-    #construct holding dictionary
-    print 'construct holding dictionary...'
-    values_dictionary={}
-    for i in np.arange(len(super_list)):
-        values_dictionary[super_list[i]]=[[],[]]
-    print values_dictionary.keys()
-    #add data to holding dictionary
-    print 'add data to holding dictionary...'
-    for row in np.arange(row_count):
-        key=''
-        for s_index in s_col:
-            key+=str(all_data[row,s_index])+' '
-            
-        key=key[:-1]
-        #print key
-        values_dictionary[key][0].append(all_data[row,x_col])
-        values_dictionary[key][1].append(all_data[row,y_col])
-
-    #plot data
-    print 'plot data...'
-    '''
-    unique_vals_string_no_Z=[]
-    for i in np.arange(1,len(unique_vals)):
-        unique_vals_string_no_Z.append([]);
-        for j in np.arange(len(unique_vals[i])):
-            unique_vals_string_no_Z[i].append(str(unique_vals[i][j]))
-'''
-
-    #Construct vector of unique values, saved as strings
-    print 'Construct vector of unique values, saved as strings...'
-    #unique_vals_string_no_Z=[]
-    #for i in np.arange(1,len(unique_vals)):
-    #    unique_vals_string_no_Z.append([]);
-    #    for j in np.arange(len(unique_vals[i])):
-    #        print str(i)+','+str(j)
-    #        unique_vals_string_no_Z.append(str(unique_vals[i][j]))
-    unique_vals_string_no_Z=[]
-    for i in np.arange(1,len(unique_vals_string)):
-        unique_vals_string_no_Z.append(unique_vals_string[i])
-    print unique_vals_string_no_Z
-    super_list_no_Z=[" ".join(i) for i in it.product(*unique_vals_string_no_Z)]
-    #for items in it.product(*unique_vals_string_no_Z):
-    #    super_list_no_Z.append(items)
-    print super_list_no_Z
-    for i in super_list_no_Z:
-        plt.figure(1)
-        for z_val in unique_vals_string[0]:
-            #z_val_str=str(z_val)
-            key=z_val+' '+i
-            print key
-            plt.plot(values_dictionary[z_val+' '+i][0],values_dictionary[z_val+' '+i][1],label=z_val)
-        plt.title(i)
-        plt.legend(loc='upper right')
-        axes = plt.gca()
-
-        ymin=0
-        ymax=1
-        axes.set_ylim([ymin,ymax])
+    if len(x_col_in)==1:
+        x_vec=[x_col_in[0],x_col_in[0]+1]
+    else:
+        x_vec=[x_col_in[0],x_col_in[1]+1]
+    if len(y_col_in)==1:
+        y_vec=[y_col_in[0],y_col_in[0]+1]
+    else:
+        y_vec=[y_col_in[0],y_col_in[1]+1]
         
-        #plt.show()
-        plt.savefig(i+'.jpg', format='jpg', dpi=200)
-        plt.clf()
+    for x_col in np.arange(x_vec[0],x_vec[1]):
+        for y_col in np.arange(y_vec[0],y_vec[1]):
+
+            #construct holding dictionary
+            print 'construct holding dictionary...'
+            values_dictionary={}
+            for i in np.arange(len(super_list)):
+                values_dictionary[super_list[i]]=[[],[]]
+            print values_dictionary.keys()
+            #add data to holding dictionary
+            print 'add data to holding dictionary...'
+            for row in np.arange(row_count):
+                key=''
+                for s_index in s_col:
+                    key+=str(all_data[row,s_index])+' '
+            
+                key=key[:-1]
+                #print key
+                values_dictionary[key][0].append(all_data[row,x_col])
+                values_dictionary[key][1].append(all_data[row,y_col])
+
+            #plot data
+            print 'plot data...'
+            '''
+            unique_vals_string_no_Z=[]
+            for i in np.arange(1,len(unique_vals)):
+                unique_vals_string_no_Z.append([]);
+                for j in np.arange(len(unique_vals[i])):
+                    unique_vals_string_no_Z[i].append(str(unique_vals[i][j]))
+        '''
+
+            #Construct vector of unique values, saved as strings
+            print 'Construct vector of unique values, saved as strings...'
+            #unique_vals_string_no_Z=[]
+            #for i in np.arange(1,len(unique_vals)):
+            #    unique_vals_string_no_Z.append([]);
+            #    for j in np.arange(len(unique_vals[i])):
+            #        print str(i)+','+str(j)
+            #        unique_vals_string_no_Z.append(str(unique_vals[i][j]))
+            unique_vals_string_no_Z=[]
+            for i in np.arange(1,len(unique_vals_string)):
+                unique_vals_string_no_Z.append(unique_vals_string[i])
+            print unique_vals_string_no_Z
+            super_list_no_Z=[" ".join(i) for i in it.product(*unique_vals_string_no_Z)]
+            #for items in it.product(*unique_vals_string_no_Z):
+            #    super_list_no_Z.append(items)
+            print super_list_no_Z
+            for i in super_list_no_Z:
+                plt.figure(1)
+                for z_val in unique_vals_string[0]:
+                    #z_val_str=str(z_val)
+                    key=z_val+' '+i
+                    print key
+                    plt.plot(values_dictionary[z_val+' '+i][0],values_dictionary[z_val+' '+i][1],label=z_val,alpha=0.5,marker='.',linestyle = 'None',ms=4)
+                plt.title(i)
+                #plt.legend(loc='best')
+                axes = plt.gca()
+
+                if len(yaxis)==2:
+                    ymin=yaxis[0]
+                    ymax=yaxis[1]
+                    axes.set_ylim([ymin,ymax])
+
+                if len(xaxis)==2:
+                    xmin=xaxis[0]
+                    xmax=xaxis[1]
+                    axes.set_xlim([xmin,xmax])
+
+        
+                #plt.show()
+                print "Saving..."
+                plt.savefig(i+"--"+str(x_col)+"-"+str(y_col)+"-"+str(s_col)+'.jpg', format='jpg', dpi=200)
+                plt.clf()
 
 
 
