58108fe881ad6bb5a207ca488f32156e09426372
Graham Thomas <graham@arylla.com>
2018-05-29 09:42:17 -0400

    Delete unnecessary lines Made file pulls and folder ID non specific to GTT computer.  It's now based on the folder you're working in.

--- PythonApplication1/FILE_master.py
+++ PythonApplication1/FILE_master.py
@@ -1,36 +1,12 @@
-#Identifies the best colorspaces and averages them together
 #####################__________LIBRARIES______________################3
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.figure as pltfig
 import os
-import winsound
-import sys
-from glob import glob
-import time
-#import piexif
-#import piexif.helper
-import string
-import copy
 import shutil
 import csv
 
-#############____________CONSTANTS_______________##########33
-
-user_comment='test_comment'
-#file=raw_input('Filename with extension: ')
-
 #############3_______PHOTO IMPORT_______##############
+directory_move="FILE_master_badPics"
 
-directory_move="C://Users//gttho//Repos//py-labs-new//PythonApplication1//FILE_master_badPics"
-
-directory_start="C://Users//gttho//Documents//Visual Studio 2017//Projects//PythonApplication1//PythonApplication1//bad_pics"
-directory_start="C://Users//gttho//Resilio Sync//Original Images//CanadaGoose//180325-180326 DC_Wash_Conc_Sweep"
-directory_start="C://Users//gttho//Resilio Sync//Original Images//CanadaGoose//180330 Refined Conc Sweep Test"
-adder_string=["50mS","75mS","100mS"]
-adder_string=[""]
-
-csv_file="C://Users//gttho//Repos//py-labs-new//PythonApplication1//Pic_import.csv"
+csv_file="Pic_import.csv"
 
 with open(csv_file,'rb') as f:
     reader=csv.reader(f)
