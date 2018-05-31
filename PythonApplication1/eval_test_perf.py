b8ea76ddee258acba547abdb5054a032bd50eb24
Graham Thomas <graham@arylla.com>
2018-05-31 16:01:53 -0400

    Modify method used to find threshold and calcualte J.
    Technique written by DS didn't guarantee maximum J and didn't use the typical method to find threshold.  It's been fixed.
    Also, the results are writted as a dict to `testperf.txt`

--- PythonApplication1/eval_test_perf.py
+++ PythonApplication1/eval_test_perf.py
@@ -1,6 +1,6 @@
 #test the performance of the your coeffs against your transcript file
 
-import os, math, sys, random
+import os, math, sys, random, json, fun
 
 
 classes = {}
@@ -18,7 +18,7 @@ for line in open("coeffs.txt"):
 
 test = []
 for line in open("transcript.txt"):
-  pieces = line.strip().split() 
+  pieces = line.strip().split(";") 
   #class
   cl = int(pieces[0])
   #filename
@@ -38,11 +38,14 @@ for line in open("transcript.txt"):
   #save the scores
   classes[cl].append(v)
   #if it's test data, add it to the test list
-  if cl in [2,3]: test.append( (dumbscore, random.uniform(0,1), cl, fn) )
+  #if cl in [2,3]: test.append( (dumbscore, random.uniform(0,1), cl, fn) )
+  if cl in [0,1,2,3]: test.append( (dumbscore, random.uniform(0,1), cl, fn) )
 
 #sort is by the first element, so ideally it'll be a block of negative samples then a block of positive samples
 test.sort()
 test.reverse()
+
+#print test
 
 npos = len(classes[2])
 nneg = len(classes[3])
@@ -53,13 +56,49 @@ negreject = nneg
 targ = 0.85
 #determine what the score threshold is.  the test list is sorted by score
 for sco, ro, cl, fn in test:
-  # print sco, fn  # debugging hack
+ # print sco, fn  # debugging hack
   if poskept >= targ*npos: break
-  if cl == 2: poskept += 1
+  if cl == 2 or cl == 0: poskept += 1
   else: negreject -= 1
   lastscore = sco
 
-print "        J score: %8.4f" % ((targ + 3*negreject/float(nneg))/4.0)
+#print "        J score: %8.4f" % ((targ + 3*negreject/float(nneg))/4.0)
 odds = math.exp(lastscore)
-print "    Threshold %%: %8.4f" % (odds/(1+odds))
-print "Threshold score: %8.4f" % lastscore
+#print "    Threshold %%: %8.4f" % (odds/(1+odds))
+#print "Threshold score: %8.4f" % lastscore
+
+perf_dict={}
+perf_dict["DM_J"]=(targ + 3*negreject/float(nneg))/4.0
+perf_dict["DM_ThreshPerc"]=odds/(1+odds)
+perf_dict["DM_ThreshScore"]=lastscore
+
+
+for cl_filter,name in zip([[0,1],[2,3],[0,1,2,3]],["train","test","tot"]):
+    diff_holder=[]
+    mark_holder=[]
+    for sco, ro, cl, fn in test:
+      if not cl in cl_filter: continue
+      diff_holder.append(sco)
+      if cl==0 or cl==2: mark_holder.append(1)
+      elif cl==1 or cl==3: mark_holder.append(0)
+    thresh,J_abs,J,sen,spec=fun.threshold_finder(diff_holder,mark_holder)
+
+
+    odds = math.exp(thresh)
+    perf_dict[name+"_"+"J"]=J
+    perf_dict[name+"_"+"Sen"]=sen
+    perf_dict[name+"_"+"Spec"]=spec
+    perf_dict[name+"_"+"n_B"]=len(mark_holder)-sum(mark_holder)
+    perf_dict[name+"_"+"n_P"]=sum(mark_holder)
+    perf_dict[name+"_"+"ThreshPerc"]=odds/(1+odds)
+    perf_dict[name+"_"+"ThreshScore"]=thresh
+
+
+    print "        J score: %8.4f" % (J)
+
+    print "    Threshold %%: %8.4f" % (odds/(1+odds))
+    print "Threshold score: %8.4f" % thresh
+
+
+with open("testperf.txt","wb") as file:
+    file.write(json.dumps(perf_dict))
