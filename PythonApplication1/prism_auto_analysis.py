fe83958a48bbcb7e4c55d847f8cdd0b9180592b8
Graham Thomas <graham@arylla.com>
2018-06-14 09:53:05 -0400

    Add more ruggedness to the code so that it can run through a number of input files Output files will now have the filename as a prefix

--- PythonApplication1/prism_auto_analysis.py
+++ PythonApplication1/prism_auto_analysis.py
@@ -19,6 +19,11 @@ serial_dict={}
 serial_dict[1]="101010101010101010110101010101"
 serial_dict[2]="010101010101010101001010101010"
 
+#optionally, combine ROIs
+roi_combo_switch=1
+roi_combo_list=[0,2,4,6,8,10,12,14,16,18]
+
+
 #Define bin constants
 bins=["wh", "lg", "mg", "dg", "bk", "r", "o", "y", "sg", "g", "tu", "cy", "az", "b", "pu","ma","pi","bc"]
 sides=["l","r"]
@@ -38,7 +43,11 @@ for dir,_,_ in os.walk(start_dir):
 
 
 #Iterate through all the csv files
+print files
 for file in files:
+    run_switch=1
+    filename=file.rpartition("\\")[2]
+
     try:
         df=pd.read_csv(file,header=0,error_bad_lines=False,warn_bad_lines=False)
         #iterate through all the ROIs, make a cotsu file, then put it into the detailed analysis
@@ -46,62 +55,92 @@ for file in files:
         #'0' - blank image
         #'1' - print image
         #'-1' - blank roi.  However, this is on a serialized image so other elements of the image have been printed onto
-        roi_index=0;
+        roi_index=1;
        
-        while (True):
+        while (run_switch):
             print "ROI_index= %i" % (roi_index)
             try:         
                 lf = open("otsu_0.txt", "wb")
+                blank_count=0
+                print_count=0
                 for index, row in df.iterrows():
-                    qq={}
-                    #export the prism data
-                    printer=True
-                    otsu_string=""
-                    for bin in bins:
-                        otsu_string+=" "+bin.upper()
-                        for side in sides:
-                            value=row["roi"+str(int(roi_index))+"_"+side+"_"+bin]
-                            #don't save line if there are nan values
-                            if math.isnan(value): printer=False
-                            otsu_string+=" "+str(value)
-                        otsu_string+="\n"
-                    if not printer: continue
-                    qq["otsu"]=otsu_string
+                    if roi_combo_switch:
+                        roi_list=roi_combo_list
+                    else:
+                        roi_list=[roi_index]
+                    for roi_idx in roi_list:
+                        qq={}
+                        #export the prism data
+                        printer=True
+                        otsu_string=""
+                        for bin in bins:
+                            otsu_string+=" "+bin.upper()
+                            for side in sides:
+                                data_key="roi"+str(int(roi_idx))+"_"+side+"_"+bin
+                           
+                                value=row[data_key]
+                                try:
+                                    value=float(value)
+                                    if math.isnan(value): printer=False
+                                except ValueError:
+                                    printer=False
+                                #print data_key,value,type(value)
+                                #don't save line if there are nan values                          
+                                otsu_string+=" "+str(value)
+                            otsu_string+="\n"
+                        if not printer: continue
+                        qq["otsu"]=otsu_string
 
-                    #export the filename
-                    fn=row["path"]
-                    qq["file"]=fn
+                        #export the filename
+                        fn=row["path"]
+                        qq["file"]=fn
 
-                    #export the set
-                    df_columns=list(df)
-                    pset=""
-                    if int(row["mark"])==0: pset="0"
-                    elif int(row["mark"])==1:
-                        if ("decimal" in df_columns) and ("roi_count" in df_columns):
-                            #Get identity of ROI via the 'decimal' input
-                            #decimal is converted to binary, which is then converted to a vector of bools
-                            roi_number=row["roi_count"]
-                            decimal_print=row["decimal"]
-                            binary_string=format(decimal_print,'b').zfill(roi_number)
-                            binary_list=list(binary_string)
-                            mark_value=int(binary_list[roi_index])
-                            if (mark_value): pset="1"
-                            else: pset="-1"
-                        elif "serial" in df_columns:
-                            binary_string=serial_dict[row["serial"]]
-                            binary_list=list(binary_string)
-                            mark_value=int(binary_list[roi_index])
-                            print row["serial"],binary_string, mark_value,roi_index
-                            if (mark_value): pset="1"
-                            else: pset="-1"
-                        else: pset="1"
-                    else: continue
-                    qq["set"]=pset
+                        #export the set
+                        df_columns=list(df)
+                        pset=""
+                        if int(row["mark"])==0: 
+                            pset="0"
+                            blank_count+=1
+                        elif int(row["mark"])==1:
+                            if ("decimal" in df_columns) and ("roi_count" in df_columns):
+                                #Get identity of ROI via the 'decimal' input
+                                #decimal is converted to binary, which is then converted to a vector of bools
+                                roi_number=row["roi_count"]
+                                decimal_print=row["decimal"]
+                                binary_string=format(decimal_print,'b').zfill(roi_number)
+                                binary_list=list(binary_string)
+                                mark_value=int(binary_list[roi_idx])
+                                if (mark_value): 
+                                    pset="1"
+                                    print_count+=1
+                                else: 
+                                    pset="-1"
+                                    blank_count+=1
+                            elif "serial" in df_columns:
+                                binary_string=serial_dict[row["serial"]]
+                                binary_list=list(binary_string)
+                                mark_value=int(binary_list[roi_idx])
+                                #print row["serial"],binary_string, mark_value,roi_idx
+                                if (mark_value):
+                                    pset="1"
+                                    print_count+=1
+                                else: 
+                                    pset="-1"
+                                    blank_count+=1
+                            else: 
+                                pset="1"
+                                print_count+=1
+                        else: continue
+                        qq["set"]=pset
 
-                    #write to text file
-                    lf.write(json.dumps(qq) + "\n")
+                        #write to text file
+                        lf.write(json.dumps(qq) + "\n")
 
                 lf.close()
+
+                if blank_count==0 or print_count==0:
+                    roi_index+=1
+                    continue
 
                 #perform logistic regression analysis
                 os.system("python make_feature_maps.py") #make giant 630x61x61 index
@@ -109,16 +148,17 @@ for file in files:
                 os.system("python eval_test_perf.py") #determine the accuracy of the model
 
                 #rename important files so they won't be overwritten later
-                os.rename('coeffs.txt', 'coeffs_'+str(int(roi_index))+'.txt')
-                os.rename('transcript.txt', 'transcript_'+str(int(roi_index))+'.txt')
-                os.rename('testperf.txt', 'testperf_'+str(int(roi_index))+'.txt')
-                os.rename('master_map.json', 'master_map_'+str(int(roi_index))+'.json')
+                os.rename('coeffs.txt', filename+'_coeffs_'+str(int(roi_index))+'.txt')
+                os.rename('transcript.txt', filename+'_transcript_'+str(int(roi_index))+'.txt')
+                os.rename('testperf.txt', filename+'_testperf_'+str(int(roi_index))+'.txt')
+                os.rename('master_map.json', filename+'_master_map_'+str(int(roi_index))+'.json')
+                if roi_combo_switch: run_switch=0
                 roi_index+=1
                 
             except KeyError:
                 break
             except WindowsError:
                 raise
-        prism_fun.serial_test(serial_dict)
+        #prism_fun.serial_test(serial_dict)
     except:
-        raise
+        continue
